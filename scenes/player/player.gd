63a06602abcb4be2859678b4718c79956585450a
Rogelio Long <rlong021@gmail.com>
2026-04-11 13:47:23 -0600

    middle of lecture stuff

---
+++ scenes/player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		print("You're pressing right")
