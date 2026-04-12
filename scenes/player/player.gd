145be90aaab829afe2326f4810fca239b88769a5
Rogelio Long <rlong021@gmail.com>
2026-04-12 17:48:12 -0600

    finished 3rd set of lectures

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,18 +1,30 @@
 extends CharacterBody2D
+class_name  Player
 
-
+@export var move_speed: float = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	position = SceneManager.player_spawn_position
+	print(move_speed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	
-	var move_speed: float = 100
-	
 	velocity = move_vector *move_speed
+	
+	if velocity.x >0:
+		$AnimatedSprite2D.play("move_right")
+	elif velocity.x <0:
+		$AnimatedSprite2D.play("move_left")
+	elif velocity.y >0:
+		$AnimatedSprite2D.play("move_down")
+	elif velocity.y <0:
+		$AnimatedSprite2D.play("move_up")
+	else:
+		$AnimatedSprite2D.stop()
+
 	
 	move_and_slide()
