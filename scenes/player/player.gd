74ff731b816b35324edbd278238b089fb601a9fc
Rogelio Long <rlong021@gmail.com>
2026-04-12 01:09:01 -0600

    update

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -8,5 +9,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		print("You're pressing right")
+	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	var move_speed: float = 100
+	
+	velocity = move_vector *move_speed
+	
+	move_and_slide()
