8bce6f241b957c13bef438278418e7305935d881
Rogelio Long <rlong021@gmail.com>
2026-04-16 22:43:13 -0600

    update block

--- scenes/puzzlebutton/puzzle_button.gd
+++ scenes/puzzlebutton/puzzle_button.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
 var bodies_on_top:int = 0
+signal pressed
+signal unpressed
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("pushable") or body is Player:
 		bodies_on_top +=1
 		if bodies_on_top == 1:
+			pressed.emit()
 			print("I have been pushed.")
 			$AnimatedSprite2D.play("pressed")
 			
@@ -14,6 +17,7 @@ func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("pushable") or body is Player:
 		bodies_on_top -=1
 		if bodies_on_top == 0:
+			unpressed.emit()
 			print("I'm no longer pushed.")
 			$AnimatedSprite2D.play("unpressed")
 			
