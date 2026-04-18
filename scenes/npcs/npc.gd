8e3094ba14be061f62d29243a84cfbe31163a793
Rogelio Long <rlong021@gmail.com>
2026-04-18 16:30:42 -0600

    npcs and dialogue added

---
+++ scenes/npcs/npc.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+
+var can_interact: bool = false
+
+@export var dialogue_lines: Array[String] = ["Hi","How are you?", "Bye"]
+var dialogue_index: int = 0
+
+func _process(delta):
+	if Input.is_action_just_pressed("interact") and can_interact:
+		if dialogue_index < dialogue_lines.size():
+			$CanvasLayer.visible = true
+			get_tree().paused = true
+		
+			$CanvasLayer/DialogueLabel.text = dialogue_lines[dialogue_index]
+			dialogue_index += 1
+		else:
+			$CanvasLayer.visible = false
+			get_tree().paused = false
+			dialogue_index = 0 
+		
+			
+		#else:
+			#$CanvasLayer.visible = true
+			#get_tree().paused = true
+			#
+	#if !can_interact:
+		#$CanvasLayer.visible = false
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -13,25 +13,33 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	move_player()
+
+	push_blocks()
+	
+	move_and_slide()
+
+func move_player():
 	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	
 	velocity = move_vector *move_speed
 	
 	if velocity.x >0:
 		$AnimatedSprite2D.play("move_right")
+		$InteractArea2D.position = Vector2(6,2)
 	elif velocity.x <0:
 		$AnimatedSprite2D.play("move_left")
+		$InteractArea2D.position = Vector2(-6,2)
 	elif velocity.y >0:
 		$AnimatedSprite2D.play("move_down")
+		$InteractArea2D.position = Vector2(0,8)
 	elif velocity.y <0:
 		$AnimatedSprite2D.play("move_up")
+		$InteractArea2D.position = Vector2(0,-5)
 	else:
 		$AnimatedSprite2D.stop()
 
-
-	#get last collision
-	#check if it's the block
-	#If block, push it.
+func push_blocks():
 	var collision: KinematicCollision2D = get_last_slide_collision()
 	
 	if collision:
@@ -45,4 +53,15 @@ func _physics_process(delta: float) -> void:
 		if collider_node.is_in_group("wall"):
 			print("I'm touching a wall")
 	
-	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("interactable"):
+		body.can_interact = true
+		print("I touched an NPC!")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("interactable"):
+		body.can_interact = false
+		print("I stopped touching an NPC!")
